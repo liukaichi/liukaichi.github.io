02853635d90fe7b41228898de8dd0bbf3c759521
Justin Liu <liukaichi@gmail.com>
2018-02-22 18:16:29 +0800

    Added smooth scrolling "work" enticing button

--- assets/js/main.js
+++ assets/js/main.js
@@ -161,6 +161,14 @@
 
 				});
 
+				$(document).on('click', 'a[href^="#"]', function (event) {
+					event.preventDefault();
+					
+					$('html, body').animate({
+						scrollTop: $($.attr(this, 'href')).offset().top
+					}, 500);
+				});
+
 	});
 
 })(jQuery);
